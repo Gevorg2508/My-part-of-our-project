8b7b6119d1effd1ce803117e89bed38d20bcc283
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2022-01-23 21:58:12 +0400

    Feat: my part of our projct

---
+++ script.js
@@ -0,0 +1,17 @@
+// function avg(a, b,...rest){
+//     let count = 0;
+//     let number = 0;
+//     for(let i = 0; i< rest.length; i++){
+//         count += rest[i];
+//         number++;
+//     }
+//     let global = a*b;
+//     return Math.trunc((count * 100)/global)+"%";
+// }
+// console.log(avg(2, 20, 15, 15));
+
+
+// function calc(num){
+//     return Math.trunc(1280/1920*num)+"px";
+// }
+// console.log(calc(855));
